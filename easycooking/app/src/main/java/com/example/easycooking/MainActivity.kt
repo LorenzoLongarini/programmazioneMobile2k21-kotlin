988a02360e46980e9ead19694833b2a96455e8ff
Lorenzo Longarini <s1089145@studenti.univpm.it>
2021-05-05 12:26:27 +0200

    risoluzione view

--- easycooking/app/src/main/java/com/example/easycooking/MainActivity.kt
+++ easycooking/app/src/main/java/com/example/easycooking/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.easycooking.view.RicetteCerca
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.bottone_registrati)
-        button.setOnClickListener{
+        val senzaReg = findViewById<TextView>(R.id.senza_registrazione)
+        senzaReg.setOnClickListener{
             val intent = Intent(this, RicetteCerca::class.java)
             //intent.putExtra(NOTE_ID, note.id)
-            //if login corretto
             startActivity(intent)
         }
     }
