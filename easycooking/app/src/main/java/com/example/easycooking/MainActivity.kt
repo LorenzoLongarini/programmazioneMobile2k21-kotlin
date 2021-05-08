ec0eed01eb774c175ac6af08a7296ba572b0e52c
ChiaraAmalia <s1087171@studenti.univpm.it>
2021-05-08 18:22:29 +0200

    commit

--- easycooking/app/src/main/java/com/example/easycooking/MainActivity.kt
+++ easycooking/app/src/main/java/com/example/easycooking/MainActivity.kt
@@ -1,34 +1,18 @@
 package com.example.easycooking
 
-import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.example.easycooking.view.RicetteCerca
-import com.google.firebase.auth.FirebaseAuth
-
+import android.view.View
 class MainActivity : AppCompatActivity() {
-
-    val LOGIN_REQUEST = 101
-    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val preferences = getSharedPreferences("login", Context.MODE_PRIVATE)
-        if (preferences.getBoolean("firstrun", true)) {
-            val intent = Intent(this@MainActivity, LoginActivity::class.java)
-            startActivityForResult(intent, LOGIN_REQUEST)
-        } else {
-            mAuth = FirebaseAuth.getInstance()
-            val currentUser = mAuth!!.getCurrentUser()
-            supportActionBar!!.setTitle(currentUser.displayName)
-        }
-
-        fun senzaReg(v: View) {
-            val intent = Intent(this, RicetteCerca::class.java)
-            startActivity(intent)
-        }
+    }
+    fun senzaReg(v: View){
+        val intent = Intent(this, RicetteCerca::class.java)
+        startActivity(intent)
     }
 }
