70f1772e6ccd9c6123a6008f87a8c107b910e377
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-11 12:27:04 +0200

    Update RicetteCerca.kt

--- easycooking/app/src/main/java/com/example/easycooking/view/RicetteCerca.kt
+++ easycooking/app/src/main/java/com/example/easycooking/view/RicetteCerca.kt
@@ -11,7 +11,12 @@ import com.example.easycooking.LoginActivity
 import com.example.easycooking.R
 import com.example.easycooking.adapter.dispensa.DefaultItemDecorator
 import com.example.easycooking.adapter.dispensa.Dispensa
+import com.example.easycooking.adapter.ricetta.Ricetta
+import com.example.easycooking.adapter.ricetta.RicettaAdapter
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter
+import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter
 
 
 class RicetteCerca : AppCompatActivity() {
@@ -22,23 +27,48 @@ class RicetteCerca : AppCompatActivity() {
         setContentView(R.layout.fragment_ricettecerca)
 
         val rv: RecyclerView =findViewById(R.id.rv)
-        rv.layoutManager= GridLayoutManager(this,2)
-        rv.addItemDecoration(
-            DefaultItemDecorator(resources.getDimensionPixelSize(R.dimen.provider_name_horizontalBig_margin),
-            resources.getDimensionPixelSize(R.dimen.provider_name_vertical_margin))
-        )
+        rv.layoutManager = GridLayoutManager(this, 2)
+        rv?.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
 
-        /*val alphaAdapter = AlphaInAnimationAdapter(RicettaAdapter(TODO("PASSARE LISTA RICETTE"))).apply {
-            // Change the durations.
-            setDuration(500)
-            // Disable the first scroll mode.
-            setFirstOnly(false)
+            // set the custom adapter to the RecyclerView
+
+            //connessione al DB
+            //creazione di un'istanza DB
+            val myDB = FirebaseFirestore.getInstance()
+            //connessione alla collection
+            val ricettario = myDB.collection("cook")
+            //creo la lista dove metterò le ricette
+            var appoggio = mutableListOf<Ricetta>()
+            //prendo le ricette e carico la lista
+            ricettario.get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        var Ricetta = document.toObject(Ricetta::class.java)
+                        appoggio.add(Ricetta)
+                    }
+                }
+            //FINE parte relativa allo scaricamento dei dati
+
+
+            rv?.addItemDecoration(
+                DefaultItemDecorator(
+                    resources.getDimensionPixelSize(R.dimen.provider_name_horizontalBig_margin),
+                    resources.getDimensionPixelSize(R.dimen.provider_name_vertical_margin)
+                )
+            )
+
+            val alphaAdapter = AlphaInAnimationAdapter(RicettaAdapter(appoggio)).apply {
+                // Change the durations.
+                setDuration(500)
+                // Disable the first scroll mode.
+                setFirstOnly(false)
+            }
+            rv?.adapter = ScaleInAnimationAdapter(alphaAdapter).apply {
+                setDuration(250)
+            }
         }
-        rv.adapter = ScaleInAnimationAdapter(alphaAdapter).apply {
-            setDuration(250)
-        }*/
-
-    }
 
     fun login(v: View) {
         val preferences = getSharedPreferences("login", Context.MODE_PRIVATE)
@@ -65,4 +95,5 @@ class RicetteCerca : AppCompatActivity() {
                 editor.apply()
             }
         }*/
+}
 }
