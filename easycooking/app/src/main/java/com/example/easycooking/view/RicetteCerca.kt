3686b1e99a8e10303193817ae3fba56af7f8fbe6
Lorenzo Longarini <s1089145@studenti.univpm.it>
2021-05-25 23:00:44 +0200

    perdoname madre por mi commit loca

--- easycooking/app/src/main/java/com/example/easycooking/view/RicetteCerca.kt
+++ easycooking/app/src/main/java/com/example/easycooking/view/RicetteCerca.kt
@@ -50,8 +50,6 @@ class RicetteCerca : Fragment(R.layout.fragment_ricettecerca) {
 
     /*override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
         super.onViewCreated(itemView, savedInstanceState)
-
-
     }*/
 
     override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
@@ -69,180 +67,163 @@ class RicetteCerca : Fragment(R.layout.fragment_ricettecerca) {
             // RecyclerView behavior
             layoutManager = GridLayoutManager(activity, 2)
             // set the custom adapter to the RecyclerView
+        }
+
+        //var appoggio = mutableListOf<Ricetta>()
+        ricettaArray = arrayListOf<Ricetta>()
+        getRicette()
+        /*val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+        val docRef = db.collection("cook").document("100")
+        docRef.get().addOnSuccessListener { document ->
+            val ric = document.toObject<Ricetta>()
+            if (ric != null) {
+                appoggio.add(0,ric)
             }
-
-            //var appoggio = mutableListOf<Ricetta>()
-            ricettaArray = arrayListOf<Ricetta>()
-            getRicette()
-            /*val db: FirebaseFirestore = FirebaseFirestore.getInstance()
-            val docRef = db.collection("cook").document("100")
-            docRef.get().addOnSuccessListener { document ->
-                val ric = document.toObject<Ricetta>()
-                if (ric != null) {
-                    appoggio.add(0,ric)
-                }
-            }
-            var o = mutableListOf<String>("k", "k")
-            val ric1 =
-                Ricetta(o, "k", "k", 0, "k", o, o, "k", "0", "k", "k", o, "k", "k", "k", o, false)
-            appoggio.add(ric1)
-
-
-
-            rv?.addItemDecoration(
-                DefaultItemDecorator(
-                    resources.getDimensionPixelSize(R.dimen.provider_name_horizontalBig_margin),
-                    resources.getDimensionPixelSize(R.dimen.provider_name_vertical_margin)
-                )
+        }
+        var o = mutableListOf<String>("k", "k")
+        val ric1 =
+            Ricetta(o, "k", "k", 0, "k", o, o, "k", "0", "k", "k", o, "k", "k", "k", o, false)
+        appoggio.add(ric1)
+        rv?.addItemDecoration(
+            DefaultItemDecorator(
+                resources.getDimensionPixelSize(R.dimen.provider_name_horizontalBig_margin),
+                resources.getDimensionPixelSize(R.dimen.provider_name_vertical_margin)
             )
-
-            val alphaAdapter = AlphaInAnimationAdapter(RicettaAdapter(appoggio)).apply {
-                // Change the durations.
-                setDuration(500)
-                // Disable the first scroll mode.
-                setFirstOnly(false)
-            }
-            rv?.adapter = ScaleInAnimationAdapter(alphaAdapter).apply {
-                setDuration(250)
-            }*/
-
+        )
+        val alphaAdapter = AlphaInAnimationAdapter(RicettaAdapter(appoggio)).apply {
+            // Change the durations.
+            setDuration(500)
+            // Disable the first scroll mode.
+            setFirstOnly(false)
         }
-
-        fun getRicette() {
-            dbref = FirebaseDatabase.getInstance().getReference("")
-            dbref.addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (snapshot.exists()) {
-                        for (ricetteSnapshot in snapshot.children) {
-                            val ricetta = ricetteSnapshot.getValue(Ricetta::class.java)
-                            ricettaArray.add(ricetta!!)
-                            if (btn != null) {
-                                btn.setOnClickListener {
-                                    var origin=orig?.selectedItem.toString()
-                                    var categ=cat?.selectedItem.toString()
-                                    ricettaArray=filtri(origin,categ,ricettaArray)
-
-                                }
-                            }
-                        }
-                        recView.adapter = context?.let { RicettaAdapter(ricettaArray, it) }
-                    }
-
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-            })
-
-
-        }
-     /*fun onItemClick(ricetta: Ricetta){
-        val fragment: Fragment = SingolaRicetta.newInstance(SingolaRicetta)
-        val transaction = activity?.supportFragmentManager!!.beginTransaction()
-        activity?.supportFragmentManager!!.findFragmentByTag("Singola Ricetta")?.let {
-            transaction.hide(
-                it
-            )
-        }
-        transaction.add(R.id.fragment_container_view_tag, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }*/
-
-
+        rv?.adapter = ScaleInAnimationAdapter(alphaAdapter).apply {
+            setDuration(250)
+        }*/
 
     }
 
+    fun getRicette() {
+        dbref = FirebaseDatabase.getInstance().getReference("")
+        dbref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (ricetteSnapshot in snapshot.children) {
+                        val ricetta = ricetteSnapshot.getValue(Ricetta::class.java)
+                        ricettaArray.add(ricetta!!)
+                        if (btn != null) {
+                            btn.setOnClickListener {
+                                var origin=orig?.selectedItem.toString()
+                                var categ=cat?.selectedItem.toString()
+                                ricettaArray=filtri(origin,categ,ricettaArray)
 
-
-        /*override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(itemView, savedInstanceState)
-        val rv: RecyclerView =findViewById(R.id.rv)
-        rv.layoutManager = GridLayoutManager(this, 2)
-
-         */
-
-        /*
-    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(itemView, savedInstanceState)
-        val rv: RecyclerView? = view?.findViewById<RecyclerView>(R.id.rv)
-
-        rv?.apply {
-            // set a LinearLayoutManager to handle Android
-            // RecyclerView behavior
-            layoutManager = GridLayoutManager(activity, 2)
-            // set the custom adapter to the RecyclerView
-
-            //connessione al DB
-            //creazione di un'istanza DB
-            val myDB = FirebaseFirestore.getInstance()
-            //connessione alla collection
-            val ricettario = myDB.collection("cook")
-            //creo la lista dove metterò le ricette
-            var appoggio = mutableListOf<Ricetta>()
-            //prendo le ricette e carico la lista
-            ricettario.get()
-                .addOnSuccessListener { documents ->
-                    for (document in documents) {
-                        var ricetta=document.toObject<Ricetta>()
-                        appoggio.add(ricetta)
+                            }
+                        }
                     }
+                    recView.adapter = context?.let { RicettaAdapter(ricettaArray, it) }
                 }
 
-
-            //FINE parte relativa allo scaricamento dei dati
-
-
-            rv?.addItemDecoration(
-                DefaultItemDecorator(
-                    resources.getDimensionPixelSize(R.dimen.provider_name_horizontalBig_margin),
-                    resources.getDimensionPixelSize(R.dimen.provider_name_vertical_margin)
-                )
-            )
-
-            val alphaAdapter = AlphaInAnimationAdapter(RicettaAdapter(appoggio)).apply {
-                // Change the durations.
-                setDuration(500)
-                // Disable the first scroll mode.
-                setFirstOnly(false)
             }
-            rv?.adapter = ScaleInAnimationAdapter(alphaAdapter).apply {
-                setDuration(250)
-            }
-        }
 
-            /*fun login(v: View) {
-        val preferences = getSharedPreferences("login", Context.MODE_PRIVATE)
-        if (preferences.getBoolean("firstrun", true)) {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivityForResult(intent, LOGIN_REQUEST)
-        } else {
-            mAuth = FirebaseAuth.getInstance()
-            val currentUser = mAuth!!.getCurrentUser()
-            supportActionBar!!.setTitle(currentUser.displayName)
-        }
-    } */
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+
+    }
+    /*fun onItemClick(ricetta: Ricetta){
+       val fragment: Fragment = SingolaRicetta.newInstance(SingolaRicetta)
+       val transaction = activity?.supportFragmentManager!!.beginTransaction()
+       activity?.supportFragmentManager!!.findFragmentByTag("Singola Ricetta")?.let {
+           transaction.hide(
+               it
+           )
+       }
+       transaction.add(R.id.fragment_container_view_tag, fragment)
+       transaction.addToBackStack(null)
+       transaction.commit()
+   }*/
 
 
 
 }
 
-            /* override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
-        super.onActivityResult(requestCode, resultCode, intent)
-        if (requestCode == LOGIN_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                val nome = intent?.extras!!.getString("nome")
-                val cognome = intent.extras!!.getString("cognome")
-                supportActionBar!!.title = "$nome $cognome"
-                val preferences = getSharedPreferences("login", MODE_PRIVATE)
-                val editor = preferences.edit()
-                editor.putBoolean("firstrun", false)
-                editor.apply()
+
+
+/*override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(itemView, savedInstanceState)
+val rv: RecyclerView =findViewById(R.id.rv)
+rv.layoutManager = GridLayoutManager(this, 2)
+ */
+
+/*
+override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+super.onViewCreated(itemView, savedInstanceState)
+val rv: RecyclerView? = view?.findViewById<RecyclerView>(R.id.rv)
+rv?.apply {
+    // set a LinearLayoutManager to handle Android
+    // RecyclerView behavior
+    layoutManager = GridLayoutManager(activity, 2)
+    // set the custom adapter to the RecyclerView
+    //connessione al DB
+    //creazione di un'istanza DB
+    val myDB = FirebaseFirestore.getInstance()
+    //connessione alla collection
+    val ricettario = myDB.collection("cook")
+    //creo la lista dove metterò le ricette
+    var appoggio = mutableListOf<Ricetta>()
+    //prendo le ricette e carico la lista
+    ricettario.get()
+        .addOnSuccessListener { documents ->
+            for (document in documents) {
+                var ricetta=document.toObject<Ricetta>()
+                appoggio.add(ricetta)
             }
-        }*/
         }
-    }*/
+    //FINE parte relativa allo scaricamento dei dati
+    rv?.addItemDecoration(
+        DefaultItemDecorator(
+            resources.getDimensionPixelSize(R.dimen.provider_name_horizontalBig_margin),
+            resources.getDimensionPixelSize(R.dimen.provider_name_vertical_margin)
+        )
+    )
+    val alphaAdapter = AlphaInAnimationAdapter(RicettaAdapter(appoggio)).apply {
+        // Change the durations.
+        setDuration(500)
+        // Disable the first scroll mode.
+        setFirstOnly(false)
+    }
+    rv?.adapter = ScaleInAnimationAdapter(alphaAdapter).apply {
+        setDuration(250)
+    }
+}
+    /*fun login(v: View) {
+val preferences = getSharedPreferences("login", Context.MODE_PRIVATE)
+if (preferences.getBoolean("firstrun", true)) {
+    val intent = Intent(this, LoginActivity::class.java)
+    startActivityForResult(intent, LOGIN_REQUEST)
+} else {
+    mAuth = FirebaseAuth.getInstance()
+    val currentUser = mAuth!!.getCurrentUser()
+    supportActionBar!!.setTitle(currentUser.displayName)
+}
+} */
+}
+    /* override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+super.onActivityResult(requestCode, resultCode, intent)
+if (requestCode == LOGIN_REQUEST) {
+    if (resultCode == RESULT_OK) {
+        val nome = intent?.extras!!.getString("nome")
+        val cognome = intent.extras!!.getString("cognome")
+        supportActionBar!!.title = "$nome $cognome"
+        val preferences = getSharedPreferences("login", MODE_PRIVATE)
+        val editor = preferences.edit()
+        editor.putBoolean("firstrun", false)
+        editor.apply()
+    }
+}*/
+}
+}*/
 
 fun filtri(origine:String, categoria:String,lista:ArrayList<Ricetta>):ArrayList<Ricetta> {
     lateinit var risultato:ArrayList<Ricetta>
@@ -272,5 +253,3 @@ fun filtri(origine:String, categoria:String,lista:ArrayList<Ricetta>):ArrayList<
     }
     return risultato
 }
-
-
