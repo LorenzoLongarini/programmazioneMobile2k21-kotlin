373c887e0776ad2f85adafd4ab2221ccb2556e4e
ChiaraAmalia <s1087171@studenti.univpm.it>
2021-06-01 23:58:27 +0200

    Revert "ok"
    This reverts commit dfbcb6fbd7d1d0f0d4097b34302b35d27a6ae6a3.

--- easycooking/app/build/generated/data_binding_trigger/debug/com/example/easycooking/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.easycooking;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- easycooking/app/build/generated/source/kapt/debug/com/example/easycooking/BR.java
+++
@@ -1,5 +0,0 @@
-package com.example.easycooking;
-
-public class BR {
-  public static final int _all = 0;
-}
