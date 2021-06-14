1dd712dc2315e1a1eb8f5aaab34041cd40de9d00
ChiaraAmalia <s1087171@studenti.univpm.it>
2021-06-14 10:21:42 +0200

    aggiunti commenti nel main activity

--- easycooking/app/src/main/java/com/example/easycooking/MainActivity.kt
+++ easycooking/app/src/main/java/com/example/easycooking/MainActivity.kt
@@ -32,80 +32,106 @@ import kotlinx.android.synthetic.main.fragment_logout.*
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         private lateinit var auth: FirebaseAuth
-        private lateinit var signInButton: SignInButton
         private lateinit var googleSignInClient: GoogleSignInClient
         private lateinit var forgotPassword: TextView
         private val TAG: String = "SignInActivity"
         private val RC_SIGN_IN = 9001
+
+        //crea il binding
         private lateinit var binding: ActivityMainBinding
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
+
+            //inizializziamo la viariabile auth attraverso FirebaseAuth.getInstance()
             auth = FirebaseAuth.getInstance()
 
+            //otteniamo la view utilizzando un layoutInflater
             binding = ActivityMainBinding.inflate(layoutInflater)
             setContentView(binding.root)
 
+            //richiamo funzione createRequest()
             createRequest()
 
+            //viene inizializzata un'istanza di FirebaseAuth
             auth = FirebaseAuth.getInstance()
 
+            //usiamo l'istanza della classe Binding per settare il click del bottone GoogleSignIn
             binding.bottoneGmail.setOnClickListener { onClick(bottone_gmail) }
 
             forgotPassword = findViewById(R.id.password_dimenticata)
             forgotPassword.setOnClickListener{startActivity(Intent(this, ResetPassword::class.java))}
+
+            //richiamiamo la funzione login()
             login()
 
         }
 
-        private fun createRequest() {
-            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+
+    private fun createRequest() {
+        //configurazione google sign-in attraverso una richiesta dell'ID utente, indirizzo e-mail e informazioni di base riguardanti il profilo utente
+        //ID e informazioni di base riguardante il profilo utente sono inclusi in DEFAULT_SIGN_IN
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
-            googleSignInClient = GoogleSignIn.getClient(this, gso)
-        }
+        //crea un GoogleSignInClient con le specifiche date da gso
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
 
-        private fun signIn() {
-            val signInIntent = googleSignInClient.signInIntent
-            startActivityForResult(signInIntent, RC_SIGN_IN)
-        }
+    /**
+     * Attraverso questa funzione viene lanciato un intent per aprire una finestra di dialogo per scegliere l'account google con cui l'utente vuole effettuare l'accesso.
+     *
+     */
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
 
+    /**
+     * Dopo che l'utente ha effettuato l'accesso, si ottiene un oggetto GoogleSignInAccount per l'utente
+     */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        // Risultato ottenuto dal lancio dell intent da GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
-                // Google Sign In was successful, authenticate with Firebase
+                // accesso con Google Sign In riuscito, l'account viene salvato su firebase
                 val account = task.getResult(ApiException::class.java)!!
                 Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
                 firebaseAuthWithGoogle(account.idToken!!)
             } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
+                // accesso con Google Sign In non riuscito
                 Log.w(TAG, "Google sign in failed", e)
             }
         }
     }
 
+    /**
+     * Dopo che l'utente ha eseguito l'accesso si ottiene un token id dall'oggetto GoogleSignInAccount
+     * l'ggetto  GoogleSignInAccount contiene quindi informazioni sull'utente
+     *
+     */
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
+                    // Sign in avvenuto con successo
                     Log.d(TAG, "signInWithCredential:success")
 
                     val user = auth.currentUser
                 } else {
-                    // If sign in fails, display a message to the user.
+                    // Sign in fallito
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
 
                 }
 
+                //viene quindi anciato un intent che ci porta all'activity vase
                 val intent = Intent(this, Base::class.java)
                 startActivity(intent)
                 finish()
@@ -115,15 +141,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             })
     }
 
+        /**
+        * Al click del bottone che ci permette di fare l'accesso con google, viene lanciata la funzione signIn()
+        *
+         */
         override fun onClick(v: View?) {
          when (v!!.id) {
             R.id.bottone_gmail -> signIn()
             }
         }
 
-        //login
+        /**
+        * Questa funzione ci permette di effettuare il login attraverso credenziali gia esistenti nel nostro database
+        *
+        */
         private fun login(){
             bottone_accedi.setOnClickListener{
+
+                //effettuiamo i controlli per vedere se l'utente inserisce o meno le credenziali dopo aver cliccato sul bottone accedi
                 if(TextUtils.isEmpty(email_accedi.text.toString()) && TextUtils.isEmpty(psw_accedi.text.toString())){
                     email_accedi.setError("Per favore inserisci la tua mail")
                     psw_accedi.setError("Per favore inserisci la tua password")
@@ -138,28 +173,39 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     return@setOnClickListener
                 }
 
+                //prendiamo l'istanza della classe FirebaseAuth per lanciare il metodo signInWithEmailAndPassword
                 auth.signInWithEmailAndPassword(email_accedi.text.toString(), psw_accedi.text.toString())
                     .addOnCompleteListener{
                         if(it.isSuccessful){
+                            //se le credenziali sono inserite correttamente, l'utente è loggato e viene lanciata l'activity riservata ai soli utenti registrati
                             val intent = Intent(this, Base::class.java)
                             startActivity(intent)
                             finish()
                         }else{
+                            //se le credenziali non sono inserite correttamente, appare un messaggio di errore
                             Toast.makeText(this@MainActivity, "Login fallito, email e/o password errati", Toast.LENGTH_LONG).show()
                         }
                     }
             }
         }
 
-        //intent
-        fun senzaReg(v: View) {
+    /**
+     * Questa funzione lancia un intent che ci permette di accedere all'area per utenti non registrati
+     *
+     */
+    fun senzaReg(v: View) {
          val intent = Intent(this, Base_nonReg::class.java)
             startActivity(intent)
-         }
-        fun registratiView(v: View) {
-            val intent2 = Intent(this, RegistrationActivity::class.java)
-         startActivity(intent2)
-        }
+    }
+
+    /**
+     * questa funzione lancia un intent che ci permette di accedere all'activity per effettuare la registrazione
+     *
+     */
+    fun registratiView(v: View) {
+        val intent2 = Intent(this, RegistrationActivity::class.java)
+        startActivity(intent2)
+    }
 
 
 }
